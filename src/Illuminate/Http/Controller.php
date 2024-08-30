ac5421d2331940a5853c0ffe605f30052b2e76bf
jospina-apptank <johnospina.apptank@gmail.com>
2024-08-30 12:43:26 -0500

    report exceptions

--- src/Illuminate/Http/Controller.php
+++ src/Illuminate/Http/Controller.php
@@ -32,10 +32,13 @@ abstract class Controller
             $this->validateUserActingAs();
             return $callback();
         }catch (ClientException $e){
+            report($e);
             return $this->responseBadRequest($e->getMessage())    ;
         } catch (\PDOException $e) {
+            report($e);
             return $this->responseBadRequest("Error in request data: Entity attributes are invalid");
         } catch (\ErrorException $e) {
+            report($e);
             return $this->responseBadRequest("Error in request data");
         } catch (NotAuthorizedException) {
             return $this->responseUnauthorized();
