e3863d00a038b02a85c70f0c479c9fcd3467995a
John Ospina <jhonospina150@gmail.com>
2025-04-26 07:53:52 -0500

    Fixed migration when an parameter is nullable with entity linked.

--- database/migrations/2024_06_05_00002_create_sync_entities.php
+++ database/migrations/2024_06_05_00002_create_sync_entities.php
@@ -118,6 +118,7 @@ return new class extends Migration {
                 default => null,
             };
 
+            $columnReference = $columnReference->nullable($parameter->isNullable);
             $columnReference = $columnReference->references(EntitySynchronizable::ATTR_ID)->on($tableRelatedName);
 
             if ($parameter->deleteOnCascade) {
