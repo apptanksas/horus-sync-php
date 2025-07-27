c72a50c658f1314902de008de579f43940a318f5
John Ospina <jhonospina150@gmail.com>
2025-07-27 11:38:51 -0500

    Fix entity data with attributes relations.

--- src/Core/Model/EntityData.php
+++ src/Core/Model/EntityData.php
@@ -37,7 +37,6 @@ class EntityData
      */
     function setEntitiesRelatedOneOfMany(string $relationName, array $relatedEntities): void
     {
-        unset($this->data[$relationName]);
         $this->data["_$relationName"] = $relatedEntities;
     }
 
@@ -51,7 +50,6 @@ class EntityData
      */
     function setEntitiesRelatedOneToOne(string $relationName, EntityData $relatedEntity): void
     {
-        unset($this->data[$relationName]);
         $this->data["_$relationName"] = $relatedEntity;
     }
 
